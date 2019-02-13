a7f22bd44bb15c74a5a8d468bd4783576f14e520
brettmadrid <brettmadrid9@gmail.com>
2019-02-13 14:25:44 -0800

    added React Router dependency, built Home and Login routes and built a Home page

--- tipease/src/App.js
+++ tipease/src/App.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react';
-import Navigation from './components/Navigation'
+import React, { Component } from "react";
+import Navigation from "./components/Navigation";
+import { Route } from "react-router-dom";
+
+import Home from './components/Home';
+import Login from './components/Login';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
+        <Home />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} /> 
+        {/* <Route path="/register" component={Register} />*/}
       </div>
     );
   }
--- tipease/src/components/Navigation.js
+++ tipease/src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink as RRNavLink } from "react-router-dom";
 import {
   Collapse,
   Navbar,
@@ -29,16 +30,27 @@ export default class Navigation extends React.Component {
   render() {
     return (
       <div>
+        {/* Reactstrap Navbar component */}
         <Navbar style={{backgroundColor: '#A26CCC'}} light expand="md">
           <NavbarBrand href="/">tipEase</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
-                <NavLink href="/components/">Home</NavLink>
+                <NavLink
+                  tag={RRNavLink}
+                  exact to="/"
+                  activeClassName="activeNavButton"
+                >Home
+                </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="/components/">About</NavLink>
+              <NavLink
+                  tag={RRNavLink}
+                  exact to="/register"
+                  activeClassName="activeNavButton"
+                >Register
+                </NavLink>
               </NavItem>
               <UncontrolledDropdown nav inNavbar>
                 <DropdownToggle nav caret>
